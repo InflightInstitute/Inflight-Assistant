0c9d025bd3f5963105ecd8ea17d47fd60c0659eb
InflightInstitute <inoel@inflightinnovations.com>
2025-02-12 15:52:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 from flask import Flask, request, jsonify
 import re
 from gtts import gTTS  # Google Text-to-Speech
